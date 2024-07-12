b9344d372b963f37ef746edc46466545966f4352
Jonathan Parker <jonathanp62@gmail.com>
2024-07-12 10:51:05 -0400

    WIP commit: Invalid AES key length: 8192 bytes

--- src/main/java/net/jmp/aes256/crypto/Encrypter.java
+++ src/main/java/net/jmp/aes256/crypto/Encrypter.java
@@ -34,7 +34,23 @@ package net.jmp.aes256.crypto;
 
 import java.io.File;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import java.util.Base64;
 import java.util.Objects;
+
+import javax.crypto.*;
+
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import net.jmp.aes256.config.Config;
 
@@ -77,6 +93,8 @@ public final class Encrypter {
 
         this.config = Objects.requireNonNull(config);
         this.options = Objects.requireNonNull(options);
+
+        // @todo Validate the cipher's character set
     }
 
     /**
@@ -109,11 +127,93 @@ public final class Encrypter {
         this.logger.entry();
 
         if (this.logger.isDebugEnabled()) {
-            this.logger.debug("Begin encrypting string: '{}'", this.options.getString());
+            this.logger.debug("Begin encrypting string    : '{}'", this.options.getString());
+            this.logger.debug("Secret key factory instance: '{}'", this.config.getSecretKeyFactoryInstance());
+            this.logger.debug("Secret key spec algorithm  : '{}'", this.config.getSecretKeySpecAlgorithm());
+            this.logger.debug("Cipher instance            : '{}'", this.config.getCipher().getInstance());
+            this.logger.debug("Cipher character set       : '{}'", this.config.getCipher().getCharacterSet());
+            this.logger.debug("PBE key spec iterations    : {}", this.config.getPbeKeySpecIterations());
+            this.logger.debug("PBE key length             : {}", this.config.getPbeKeySpecKeyLength());
         }
 
         final Salter salter = new Salter(this.config);
         final String salt = salter.getSalt(this.options.getUserId());
+
+        /* Set up the initialization vector */
+
+        final SecureRandom secureRandom = new SecureRandom();
+        final byte[] initializationVector = new byte[16];
+
+        secureRandom.nextBytes(initializationVector);
+
+        final IvParameterSpec ivParameterSpec = new IvParameterSpec(initializationVector);
+
+        /* Set up the secret key */
+
+        SecretKeyFactory secretKeyFactory;
+
+        try {
+            secretKeyFactory = SecretKeyFactory.getInstance(this.config.getSecretKeyFactoryInstance());
+        } catch (final NoSuchAlgorithmException nsae) {
+            this.logger.catching(nsae);
+            throw new RuntimeException(nsae.getMessage(), nsae);    // @todo CryptographyException?
+        }
+
+        final KeySpec keySpec = new PBEKeySpec(
+                this.options.getPassword().toCharArray(),
+                salt.getBytes(),
+                this.config.getPbeKeySpecIterations(),
+                this.config.getPbeKeySpecKeyLength()
+        );
+
+        SecretKey secretKey;
+
+        try {
+            secretKey = secretKeyFactory.generateSecret(keySpec);
+        } catch (final InvalidKeySpecException ikse) {
+            this.logger.catching(ikse);
+            throw new RuntimeException(ikse.getMessage(), ikse);    // @todo CryptographyException?
+        }
+
+        final SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getEncoded(), this.config.getSecretKeySpecAlgorithm());
+
+        /* Set up the cipher */
+
+        Cipher cipher;
+
+        try {
+            cipher = Cipher.getInstance(this.config.getCipher().getInstance());
+        } catch (final NoSuchAlgorithmException | NoSuchPaddingException e) {
+            this.logger.catching(e);
+            throw new RuntimeException(e.getMessage(), e);    // @todo CryptographyException?
+        }
+
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+        } catch (final InvalidKeyException | InvalidAlgorithmParameterException e) {
+            this.logger.catching(e);
+            throw new RuntimeException(e.getMessage(), e);    // @todo CryptographyException?
+        }
+
+        /* Perform the encryption */
+
+        byte[] cipherText;
+
+        try {
+            cipherText = cipher.doFinal(this.options.getString().getBytes(this.config.getCipher().getCharacterSet()));
+        } catch (final IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException e) {
+            this.logger.catching(e);
+            throw new RuntimeException(e.getMessage(), e);    // @todo CryptographyException?
+        }
+
+        final byte[] encryptedData = new byte[initializationVector.length + cipherText.length];
+
+        System.arraycopy(initializationVector, 0, encryptedData, 0, initializationVector.length);
+        System.arraycopy(cipherText, 0, encryptedData, initializationVector.length, cipherText.length);
+
+        final String result = Base64.getEncoder().encodeToString(encryptedData);
+
+        System.out.println(result);
 
         this.logger.exit();
     }
